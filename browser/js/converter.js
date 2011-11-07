4d04ca8c1ab22c77584882157bcfa235f2c34440
Richard Klancer <rpk@pobox.com>
2011-11-07 16:41:43 -0500

    Clarify possible 'this' confusion in step

--- browser/js/converter.js
+++ browser/js/converter.js
@@ -23,33 +23,33 @@ require._core = {
 require.resolve = (function () {
     return function (x, cwd) {
         if (!cwd) cwd = '/';
-        
+
         if (require._core[x]) return x;
         var path = require.modules.path();
         var y = cwd || '.';
-        
+
         if (x.match(/^(?:\.\.?\/|\/)/)) {
             var m = loadAsFileSync(path.resolve(y, x))
                 || loadAsDirectorySync(path.resolve(y, x));
             if (m) return m;
         }
-        
+
         var n = loadNodeModulesSync(x, y);
         if (n) return n;
-        
+
         throw new Error("Cannot find module '" + x + "'");
-        
+
         function loadAsFileSync (x) {
             if (require.modules[x]) {
                 return x;
             }
-            
+
             for (var i = 0; i < require.extensions.length; i++) {
                 var ext = require.extensions[i];
                 if (require.modules[x + ext]) return x + ext;
             }
         }
-        
+
         function loadAsDirectorySync (x) {
             x = x.replace(/\/+$/, '');
             var pkgfile = x + '/package.json';
@@ -69,10 +69,10 @@ require.resolve = (function () {
                     if (m) return m;
                 }
             }
-            
+
             return loadAsFileSync(x + '/index');
         }
-        
+
         function loadNodeModulesSync (x, start) {
             var dirs = nodeModulesPathsSync(start);
             for (var i = 0; i < dirs.length; i++) {
@@ -82,23 +82,23 @@ require.resolve = (function () {
                 var n = loadAsDirectorySync(dir + '/' + x);
                 if (n) return n;
             }
-            
+
             var m = loadAsFileSync(x);
             if (m) return m;
         }
-        
+
         function nodeModulesPathsSync (start) {
             var parts;
             if (start === '/') parts = [ '' ];
             else parts = path.normalize(start).split('/');
-            
+
             var dirs = [];
             for (var i = parts.length - 1; i >= 0; i--) {
                 if (parts[i] === 'node_modules') continue;
                 var dir = parts.slice(0, i + 1).join('/') + '/node_modules';
                 dirs.push(dir);
             }
-            
+
             return dirs;
         }
     };
@@ -114,9 +114,9 @@ require.alias = function (from, to) {
         res = require.resolve(from, '/');
     }
     var basedir = path.dirname(res);
-    
+
     var keys = Object_keys(require.modules);
-    
+
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
         if (key.slice(0, basedir.length + 1) === basedir + '/') {
@@ -134,7 +134,7 @@ require.define = function (filename, fn) {
         ? ''
         : require.modules.path().dirname(filename)
     ;
-    
+
     var require_ = function (file) {
         return require(file, dirname)
     };
@@ -144,7 +144,7 @@ require.define = function (filename, fn) {
     require_.modules = require.modules;
     require_.define = require.define;
     var module_ = { exports : {} };
-    
+
     require.modules[filename] = function () {
         require.modules[filename]._cached = module_.exports;
         fn.call(
@@ -272,7 +272,7 @@ path = normalizeArray(filter(path.split('/'), function(p) {
   if (path && trailingSlash) {
     path += '/';
   }
-  
+
   return (isAbsolute ? '/' : '') + path;
 };
 
@@ -873,73 +873,69 @@ require.define("/runtime/step.js", function (require, module, exports, __dirname
     function Step(sequence) {
       var _ref;
       this.sequence = sequence;
-      this.paneDefs = [];
+      this.panes = [];
       this.page = null;
       this.index = null;
       if (((_ref = this.sequence) != null ? _ref.type : void 0) === "InstructionSequence") {
         this.beforeText = this.sequence.text;
       }
     }
-    Step.prototype.addImagePane = function(_arg) {
-      var attribution, index, license, url;
-      url = _arg.url, license = _arg.license, attribution = _arg.attribution, index = _arg.index;
-      return this.paneDefs[index] = {
-        toHash: this.getImagePaneHash,
-        url: url,
-        license: license,
-        attribution: attribution
-      };
-    };
-    Step.prototype.getImagePaneHash = function() {
-      return {
-        type: 'image',
-        path: this.url,
-        caption: "" + this.license + " " + this.attribution
-      };
-    };
-    Step.prototype.addGraphPane = function(_arg) {
-      var datadefRef, index, title, xAxis, yAxis;
-      title = _arg.title, datadefRef = _arg.datadefRef, xAxis = _arg.xAxis, yAxis = _arg.yAxis, index = _arg.index;
-      return this.paneDefs[index] = {
-        toHash: this.getGraphPaneHash,
-        title: title,
-        datadefRef: datadefRef,
-        xAxis: xAxis,
-        yAxis: yAxis
-      };
-    };
-    Step.prototype.getGraphPaneHash = function() {
-      return {
-        type: 'graph',
-        title: this.title,
-        xAxis: this.xAxis.getUrl(),
-        yAxis: this.yAxis.getUrl(),
-        annotations: [],
-        data: this.datadefRef != null ? [this.datadefRef.datadef.name] : []
-      };
-    };
-    Step.prototype.addTablePane = function(_arg) {
-      var datadefRef, index;
-      datadefRef = _arg.datadefRef, index = _arg.index;
-      return this.paneDefs[index] = {
-        toHash: this.getTablePaneHash,
-        type: 'graph',
-        datadefRef: datadefRef
-      };
-    };
-    Step.prototype.getTablePaneHash = function() {
-      return {
-        type: 'table',
-        data: this.datadefRef.datadef.name,
-        annotations: []
-      };
-    };
     Step.prototype.setIndex = function(index) {
       this.index = index;
       return this.index;
     };
     Step.prototype.getUrl = function() {
       return "" + (this.page.getUrl()) + "/step/" + this.index;
+    };
+    Step.prototype.addImagePane = function(_arg) {
+      var attribution, index, license, url;
+      url = _arg.url, license = _arg.license, attribution = _arg.attribution, index = _arg.index;
+      return this.panes[index] = {
+        url: url,
+        license: license,
+        attribution: attribution,
+        toHash: function() {
+          return {
+            type: 'image',
+            path: this.url,
+            caption: "" + this.license + " " + this.attribution
+          };
+        }
+      };
+    };
+    Step.prototype.addGraphPane = function(_arg) {
+      var datadefRef, index, title, xAxis, yAxis;
+      title = _arg.title, datadefRef = _arg.datadefRef, xAxis = _arg.xAxis, yAxis = _arg.yAxis, index = _arg.index;
+      return this.panes[index] = {
+        title: title,
+        datadefRef: datadefRef,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        toHash: function() {
+          return {
+            type: 'graph',
+            title: this.title,
+            xAxis: this.xAxis.getUrl(),
+            yAxis: this.yAxis.getUrl(),
+            annotations: [],
+            data: this.datadefRef != null ? [this.datadefRef.datadef.name] : []
+          };
+        }
+      };
+    };
+    Step.prototype.addTablePane = function(_arg) {
+      var datadefRef, index;
+      datadefRef = _arg.datadefRef, index = _arg.index;
+      return this.panes[index] = {
+        datadefRef: datadefRef,
+        toHash: function() {
+          return {
+            type: 'table',
+            data: this.datadefRef.datadef.name,
+            annotations: []
+          };
+        }
+      };
     };
     Step.prototype.getPaneKey = function(numPanes, index) {
       if (numPanes === 1) {
@@ -952,22 +948,17 @@ require.define("/runtime/step.js", function (require, module, exports, __dirname
     };
     Step.prototype.toHash = function() {
       var panesHash;
-      panesHash = null;
-      if (this.paneDefs.length === 1) {
-        panesHash = {
-          single: this.paneDefs[0].toHash()
-        };
-      } else if (this.paneDefs.length === 2) {
-        panesHash = {
-          top: this.paneDefs[0].toHash(),
-          bottom: this.paneDefs[1].toHash()
-        };
-      }
+      panesHash = this.panes.length === 1 ? {
+        single: this.panes[0].toHash()
+      } : this.panes.length === 2 ? {
+        top: this.panes[0].toHash(),
+        bottom: this.panes[1].toHash()
+      } : void 0;
       return {
         url: this.getUrl(),
         activityPage: this.page.getUrl(),
-        paneConfig: this.paneDefs.length === 2 ? 'split' : 'single',
-        panes: panesHash,
+        paneConfig: this.panes.length === 2 ? 'split' : 'single',
+        panes: panesHash != null ? panesHash : null,
         isFinalStep: true,
         nextButtonShouldSubmit: true,
         beforeText: this.beforeText
--- src/runtime/step.coffee
+++ src/runtime/step.coffee
@@ -1,38 +1,11 @@
 exports.Step = class Step
 
   constructor: (@sequence) ->
-    @paneDefs = []
+    @panes = []
     # these need to be set later
     @page  = null
     @index = null
     @beforeText = @sequence.text if @sequence?.type == "InstructionSequence"
-
-  addImagePane: ({ url, license, attribution, index }) ->
-    @paneDefs[index] = { toHash: @getImagePaneHash, url, license, attribution }
-
-  getImagePaneHash: ->
-    type:    'image'
-    path:    @url
-    caption: "#{@license} #{@attribution}"
-
-  addGraphPane: ({ title, datadefRef, xAxis, yAxis, index }) ->
-    @paneDefs[index] = { toHash: @getGraphPaneHash, title, datadefRef, xAxis, yAxis }
-
-  getGraphPaneHash: ->
-    type:        'graph'
-    title:       @title
-    xAxis:       @xAxis.getUrl()
-    yAxis:       @yAxis.getUrl()
-    annotations: []
-    data:        if @datadefRef? then [@datadefRef.datadef.name] else []
-
-  addTablePane: ({ datadefRef, index }) ->
-    @paneDefs[index] = { toHash: @getTablePaneHash, type: 'graph', datadefRef }
-
-  getTablePaneHash:  ->
-    type:         'table'
-    data:         @datadefRef.datadef.name
-    annotations:  []
 
   setIndex: (@index) ->
     @index
@@ -40,23 +13,53 @@ exports.Step = class Step
   getUrl: ->
     "#{@page.getUrl()}/step/#{@index}"
 
+  addImagePane: ({ url, license, attribution, index }) ->
+    @panes[index] = {
+      url, license, attribution,
+      toHash: ->
+        type:    'image'
+        path:    @url
+        caption: "#{@license} #{@attribution}"
+    }
+
+  addGraphPane: ({ title, datadefRef, xAxis, yAxis, index }) ->
+    @panes[index] = {
+      title, datadefRef, xAxis, yAxis,
+      toHash: ->
+        type:        'graph'
+        title:       @title
+        xAxis:       @xAxis.getUrl()
+        yAxis:       @yAxis.getUrl()
+        annotations: []
+        data:        if @datadefRef? then [@datadefRef.datadef.name] else []
+    }
+
+  addTablePane: ({ datadefRef, index }) ->
+    @panes[index] = {
+      datadefRef,
+      toHash: ->
+        type:         'table'
+        data:         @datadefRef.datadef.name
+        annotations:  []
+    }
+
   getPaneKey: (numPanes, index) ->
     if numPanes == 1 then "single" else if index == 0 then "top" else "bottom"
 
   toHash: ->
-    panesHash = null
-    if @paneDefs.length == 1
-      panesHash =
-        single: @paneDefs[0].toHash()
-    else if @paneDefs.length == 2
-      panesHash =
-        top:    @paneDefs[0].toHash()
-        bottom: @paneDefs[1].toHash()
+
+    panesHash = if @panes.length == 1 then {
+      single: @panes[0].toHash()
+    } else if @panes.length == 2 then {
+      top:    @panes[0].toHash()
+      bottom: @panes[1].toHash()
+    }
+
     return {
       url:                    this.getUrl()
       activityPage:           @page.getUrl()
-      paneConfig:             if  @paneDefs.length == 2 then 'split' else 'single',
-      panes:                  panesHash
+      paneConfig:             if  @panes.length == 2 then 'split' else 'single',
+      panes:                  panesHash ? null
       isFinalStep:            true
       nextButtonShouldSubmit: true
       beforeText:             @beforeText
